c512387c61bf8f50996c6f25f1eb9bf83da00d1f
Deva Midhun <devamidhun.3c.kmbvm@gmail.com>
2024-05-07 16:30:01 +0530

    site under maintenence bc moving to ssg5

--- scripts/navigation.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-    fetch('/components/navigation.html')
-        .then(response => response.text())
-        .then(data => {
-            document.body.innerHTML += data;
-        })
-        .catch(error => console.error('Unable to fetch footer.', error));
-});
