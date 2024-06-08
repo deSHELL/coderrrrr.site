a34cd3d63f541ead161c7d279f6d47cc61e89eb9
Deva Midhun <dev@deva.is-cool.dev>
2024-06-08 12:05:44 +0000

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 rm -rf docs/.* docs/*
 ./bin/ssg5 src/ docs/ "Coderrrrr's Portfolio" "https://coderrrrr.site"
 echo "coderrrrr.site" > docs/CNAME
+echo "e7c6d036e73243a588eafb3327db84d7" > docs/e7c6d036e73243a588eafb3327db84d7
