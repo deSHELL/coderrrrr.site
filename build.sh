ef525b3e1bdff850ea55ed057d4d4d8292253f7a
Deva Midhun <admin@coderrrrr.site>
2024-08-25 17:15:21 +0530

    update

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@ rm -rf docs/.* docs/* docs/.files
 ./bin/ssg5 src/ docs/ "Coderrrrr's Portfolio" "https://coderrrrr.site"
 echo "coderrrrr.site" > docs/CNAME
 echo "e7c6d036e73243a588eafb3327db84d7" > docs/e7c6d036e73243a588eafb3327db84d7
+rm docs/index.js
+uglifyjs src/index.js -o docs/index.js --compress --mangle
--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const preElement = document.querySelector('pre');
-    const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-
-    async function fetchAsciiArt(filename) {
-      try {
-        const response = await fetch(filename);
-        if (!response.ok) {
-          throw new Error(`Failed to fetch ${filename}`);
-        }
-        return await response.text();
-      } catch (error) {
-        console.error('Error loading ASCII art:', error);
-        return ''; // Return an empty string on error
-      }
+const preElement = document.querySelector('pre');
+const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+async function fetchAsciiArt(filename) {
+  try {
+    const response = await fetch(filename);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch ${filename}`);
     }
-
-    async function displayAsciiArt() {
-      const bigAsciiArt = await fetchAsciiArt('bigAsciiArt.txt');
-      const smallAsciiArt = await fetchAsciiArt('smallAsciiArt.txt');
-
-      if (isMobileDevice) {
-        preElement.textContent = smallAsciiArt;
-      } else {
-        preElement.textContent = bigAsciiArt;
-      }
-    }
-
-    displayAsciiArt();
-});
+    return await response.text();
+  } catch (error) {
+    console.error('Error loading ASCII art:', error);
+    return ''; // Return an empty string on error
+  }
+}
+async function displayAsciiArt() {
+  const bigAsciiArt = await fetchAsciiArt('bigAsciiArt.txt');
+  const smallAsciiArt = await fetchAsciiArt('smallAsciiArt.txt');
+  if (isMobileDevice) {
+    preElement.textContent = smallAsciiArt;
+  } else {
+    preElement.textContent = bigAsciiArt;
+  }
+}
+displayAsciiArt();
